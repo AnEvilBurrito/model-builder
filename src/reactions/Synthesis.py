02486b7ba1b1be3998d776f5e1aa8addabc3dc82
Dawson <ylan0001@student.monash.edu>
2021-08-05 23:30:45 +1000

    Update Synthesis.py

--- src/reactions/Synthesis.py
+++ src/reactions/Synthesis.py
@@ -4,7 +4,22 @@ class Synthesis(Reactions):
 
     def __init__(self, backwardSpecies, KSyn: float = 0.01):
         super().__init__("None", backwardSpecies)
-        self.KSyn = KSyn
+        self.type = 'Syn'
+        self.params = {
+            'ksyn': KSyn
+        }
+        
+        self.noBackward = True
+        self.__renameParams()
+
+    def __renameParams(self):
+        return super().__renameParams()
+
+    def computeForward(self, stateVars: dict):
+        return super().computeForward(stateVars)
+
+    def computeBackward(self, stateVars: dict):
+        return super().computeBackward(stateVars)
 
     def getEqHeaderStr(self, index):
         return " => {backward} :R{i}".format(backward=self.bs[0], i=index)
@@ -14,7 +29,4 @@ class Synthesis(Reactions):
 
     def getBackwardEqStr(self, index):
         return super().getBackwardEqStr(index)
-
-    def getParams(self, index):
-        return {"Ksyn{i}".format(i=index): self.KSyn}
 
